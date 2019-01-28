5a86793e57cd12d89b558f37a2882ee5d4ab51ab
bebora <simone.rizzo@live.com>
2019-01-28 20:26:34 +0100

    Add files

---
+++ main.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+from telegram import Bot, InputMediaPhoto
+import json
+import logging
+import sys
+from updatedb import MeshUpdater, SafeGet
+from PIL import Image, ImageEnhance
+from io import BytesIO
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+def load_tokens():
+    try:
+        with open('tokens.json', 'r') as fp:
+            tokens = json.load(fp)
+            bot_token = tokens['bot_token']
+            users_ids = tokens['users_id']
+    except FileNotFoundError:
+        print("""
+            Create a tokens.json file with:
+            -bot_token        string
+            -users_id         list of strings
+            """)
+        sys.exit()
+    except KeyError:
+        print("""
+            tokens.json lacks something between:
+            -bot_token        string
+            -users_id         list of strings
+            """)
+        sys.exit()
+    return bot_token, users_ids
+def paste_not_avail(image):
+    na = Image.open('not_available.png')
+    offset = (430-320) // 2
+    image.paste(na, (offset, offset), na)
+    return image
+
+def desaturate(image):
+    return ImageEnhance.Color(image).enhance(0.25)
+
+def filtered_media(url, caption, img_filter):
+    r = SafeGet.get(url)
+    img = Image.open(BytesIO(r.content))
+    img = img_filter(img)
+    bio = BytesIO()
+    bio.name = url.split('/')[-1].lower()
+    img.save(bio, 'JPEG')
+    bio.seek(0)
+    return InputMediaPhoto(bio, caption=caption)
+    
+def send_album(bot, chat_ids, items, caption, img_filter=None):
+    if img_filter is None:
+        medias = [InputMediaPhoto(i['img_src'],
+                  caption=caption.format(i['name'])) for i in items]
+    else:
+        medias = [filtered_media(
+                    i['img_src'], 
+                    img_filter=paste_not_avail,
+                    caption=caption.format(i['name'])) for i in items]
+    for chat in chat_ids:
+        bot.sendMediaGroup(chat_id = chat, media=medias, timeout=30)
+
+def announce_new(bot, chat_ids, items):
+    if items == []:
+        logging.info("Nothing new")
+        return
+    for item in items:
+        logging.info("New item:\n"+json.dumps(item, indent=3))
+    while len(items) > 0:
+        temp = items[:10]
+        items = items[10:]
+        send_album(bot, chat_ids, temp,
+                   caption="{} is now available")
+    return
+
+def announce_removed(bot, chat_ids, items):
+    if items == []:
+        logging.info("Nothing removed")
+        return
+    for item in items:
+        logging.info("Removed item:\n"+json.dumps(item, indent=3))
+    while len(items) > 0:
+        temp = items[:10]
+        items = items[10:]
+        send_album(bot, chat_ids, temp,
+                   caption="{} isn't available anymore",
+                   img_filter=desaturate)
+    return
+
+
+def main(mesh_id=671):
+    updater = MeshUpdater(mesh_id=mesh_id)
+    changes = updater.run()
+    tokens = load_tokens()
+    bot = Bot(tokens[0])
+    chats = tokens[1]
+    announce_new(bot, chats, changes['new'])
+    announce_removed(bot, chats, changes['removed'])
+
+
+if __name__ == "__main__":
+    main()
---
+++ updatedb.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+import json
+import requests
+import time
+import logging
+import sys
+import traceback
+from tinydb import TinyDB, Query
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+class SafeGet():
+    def __init__(self):
+        pass
+    @staticmethod
+    def get(*args, **kwargs):
+        try:
+            r = requests.get(*args, **kwargs)
+        except Exception:
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
+            time.sleep(2)
+            try:
+                r = requests.get(*args, **kwargs)
+            except Exception:
+                exc_info = sys.exc_info()
+                traceback.print_exception(*exc_info)
+                sys.exit()
+        finally:
+            return r
+
+class MeshUpdater():
+    def __init__(self, mesh_id=671):
+        self.mesh_id = mesh_id
+
+    def run(self):
+        popup_request = SafeGet.get(
+            'http://zcube.vip/index.php?route=zpn/popup_product/info&product_id={}'
+            .format(self.mesh_id)
+        )
+        popup_html = BeautifulSoup(popup_request.text, 'lxml')
+        choices = []
+        color_div = popup_html \
+            .find('div', attrs={'id': 'JpopColor'}) \
+            .find('div', attrs={'class': 'col-xs-12'})
+        for span in color_div.findAll('span'):
+            choices.append({
+                'value': span['data-value'],
+                'name': span.find('img')['title'].strip(),
+                'img_src': span.find('img')['src']
+                           .strip()
+                           .replace('70x70', '430x430')
+            })
+        if len(choices) == 0:
+            logging.info("Nothing changed")
+            return {'new':[], 'removed':[]}
+        db = TinyDB('colors.json')
+        Color = Query()
+        db.update({'valid': False, 'new': False})
+
+        for item in choices:
+            result = db.search(Color.name == item['name'])
+            if result == []:
+                item['valid'] = True
+                item['new'] = True
+                db.insert(item)
+            else:
+                db.update({'valid': True}, Color.name == item['name'])
+
+        new = db.search(Color.new == True)
+        removed = db.search(Color.valid == False)
+        db.remove(Color.valid == False)
+        return {'new': new, 'removed': removed}
+
+
+
+
+
+    
